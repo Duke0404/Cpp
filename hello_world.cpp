7e2f9e67930b9c0271be8706382dffd7bb5542db
Duke0404 <devansh0404@gmail.com>
2021-03-11 14:07:02 +0100

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Input your name" << endl;
+    cin >> name;
+    cout << endl << "Nice to meet you " << name << "!";
+
+return 0;
+}
